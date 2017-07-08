940115d1a99132acd0d5afd637fab9d31df19d5d
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-08 14:47:22 +0530

    Adding GridLayout and BasicPhrases apps

---
+++ BasicPhrases/app/src/main/java/com/goyal/basicphrases/MainActivity.java
@@ -0,0 +1,30 @@
+package com.goyal.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.goyal.basicphrases");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
